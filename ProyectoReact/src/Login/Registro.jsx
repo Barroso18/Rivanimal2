bdcf725b522a4574a284c48bf5a57e12ea708683
Barroso18 <david.barrosomartinez@gmail.com>
2025-03-19 16:38:07 +0100

    continuamos con registro

--- ProyectoReact/src/Login/Registro.jsx
+++ ProyectoReact/src/Login/Registro.jsx
@@ -5,28 +5,57 @@ import ServicioUsuario from '../servicios/servicioUsuarios';
 import bcrypt from 'bcryptjs';
 import axios from 'axios';
 const Registro = () => {
-     const [usuario, setUsuario] = useState('');
-      const [password, setPassword] = useState('');
-      const [contrasena, setContrasena] = useState('');
-      const [mensaje, setMensaje] = useState('');
+    const [nombre, setNombre] = useState('');
+    const [apellido1, setApellido1] = useState('');
+    const [apellido2, setApellido2] = useState('');
+    const [contrasena, setContrasena] = useState('');
+    const [nombreUsuario, setNombreUsuario] = useState('');
+    const [roles, setRoles] = useState('');
+    const [dni, setDni] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [email, setEmail] = useState('');
+    const [direccion, setDireccion] = useState('');
+    const [foto, setFoto] = useState('');
+    const [mensaje, setMensaje] = useState('');
       const [error, setError] = useState('');
-    const handleSubmit = async (e) => {
+
+    
+    const controlaRegistro = async (e) => {
   
         e.preventDefault();
     };
     return (
         <div>
           <h2>Registro</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label><strong>Usuario: </strong></label>
+          <form onSubmit={controlaRegistro}>
+          <div>
+              <label><strong>Nombre: </strong></label>
               <input
                 type="text"
-                value={usuario}
-                onChange={(e) => setUsuario(e.target.value)}
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
                 required
               />
             </div>
+            <div>
+              <label><strong>Apellido 1: </strong></label>
+              <input
+                type="text"
+                value={apellido1}
+                onChange={(e) => setApellido1(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label><strong>Apellido 2: </strong></label>
+              <input
+                type="text"
+                value={apellido2}
+                onChange={(e) => setApellido2(e.target.value)}
+                required
+              />
+            </div>
+            
             <div>
               <label><strong>Password: </strong></label>
               
@@ -37,6 +66,15 @@ const Registro = () => {
                 required
               />
               
+            </div>
+            <div>
+              <label><strong>Nombre usuario: </strong></label>
+              <input
+                type="text"
+                value={nombreUsuario}
+                onChange={(e) => setNombreUsuario(e.target.value)}
+                required
+              />
             </div>
             {error && <p style={{ color: 'red' }}>{error}</p>}
             {mensaje && <p>{mensaje}</p>}
