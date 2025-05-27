1045b5db775ee1e5fc08a5fb1f55f4bb088daf5b
Barroso18 <david.barrosomartinez@gmail.com>
2025-05-27 20:23:48 +0200

    import react

--- ProyectoReact/src/main.jsx
+++ ProyectoReact/src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import React, { createContext, useContext, useState } from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
