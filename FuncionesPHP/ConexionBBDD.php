db8b2b2ba32cf97282bbc667d929e21c9cf2ac0f
Barroso18 <david.barrosomartinez@gmail.com>
2025-05-27 13:42:40 +0200

    2

--- FuncionesPHP/ConexionBBDD.php
+++ FuncionesPHP/ConexionBBDD.php
@@ -14,5 +14,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
+}else{
+    echo "Conexion correcta";
 }
 ?>
